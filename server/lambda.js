3ba977ba1a937497f67b5ee2604638c4ee947301
George Wang <gkwang@otago.local>
2025-08-24 14:49:39 -0700

    fix: netlify route

--- server/lambda.js
+++ server/lambda.js
@@ -1,4 +1,4 @@
-import serverless from "serverless-http";
-import app from "./index.js";
+const serverless = require("serverless-http");
+const app = require("./index.js");
 
-export const handler = serverless(app);
+module.exports.handler = serverless(app);
